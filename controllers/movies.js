75da1f1b751b965b45020a749927348b88e7d008
Alberto Gil <agildav@gmail.com>
2018-08-28 16:43:23 -0400

    User can now add movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,14 +2,52 @@
 
 var Movies = require("../models/movies");
 
-module.exports = function(router) {
+module.exports = router => {
     //  Movie listing
-    Movies.getMovies((err, movies) => {
-        if (err) res.send(err);
-        else {
-            router.get("/", function(req, res) {
+    router.get("/", (req, res) => {
+        Movies.getMovies((err, movies) => {
+            if (err) res.send(err);
+            else {
                 //  Wrap in object the movies
                 res.render("movies", { movies });
+            }
+        });
+    });
+
+    //  Add movie form
+    router.get("/add", (req, res) => {
+        res.render("add-movies");
+    });
+
+    // POST Add movie request
+    router.post("/add", (req, res) => {
+        const newMovie = {
+            title: req.body.title.trim(),
+            release_date: req.body.release_date.trim(),
+            genre: req.body.genre.trim(),
+            plot: req.body.plot.trim(),
+            director: req.body.director.trim(),
+            trailer: req.body.trailer.trim(),
+            cover: req.body.cover.trim()
+        };
+
+        if (
+            !newMovie.title ||
+            !newMovie.director ||
+            !newMovie.release_date ||
+            !newMovie.genre ||
+            !newMovie.plot
+        ) {
+            const error = { msg: "Missing movie info" };
+            res.render("add-movies", { error });
+        } else {
+            Movies.addMovies(newMovie, (err, movies) => {
+                if (err) {
+                    const error = { msg: "Could not add movie" };
+                    res.render("add-movies", { error });
+                } else {
+                    res.redirect("/movies");
+                }
             });
         }
     });
